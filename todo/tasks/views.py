0db5e7460c4145b2dac67a790ccda824dffacfd8
Erik <erik.mirai@outlook.com>
2020-07-03 12:11:17 +0300

    styling complete

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -18,14 +18,14 @@ def tasks(request):
 
 def update_task(request, pk):
     task = Task.objects.get(id=pk)
-    form = TaskForm(instance=task)
+    update_form = TaskForm(instance=task)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'update_form': update_form}
     return render(request, 'tasks/update_task.html', context)
 
 
